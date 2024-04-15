6e1ff2e67078f650265161937d123a06dd4195e1
parti26 <partosreka26@gmail.com>
2024-04-15 23:14:42 +0200

    f

--- practice.js
+++ practice.js
@@ -3,7 +3,7 @@ const lettersArr = alphabet.split("");
 function giveLettersScore(){
     let lettersWithValues = [];
     for (let i = 0; i < lettersArr.length; i++){
-        lettersArr[i].push(lettersWithValues);
+        lettersWithValues.push(lettersArr[i]+i);
     }
     return lettersWithValues;
 }
