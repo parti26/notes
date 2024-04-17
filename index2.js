d5a3ea3c99689cbe44a87f8bde17cd35fa71decf
parti26 <partosreka26@gmail.com>
2024-04-17 10:02:03 +0200

    d

--- index2.js
+++ index2.js
@@ -18,7 +18,7 @@ button.addEventListener('click', () => {
     else if (num % 3 === 0){
         li.innerText = `${num}:Fizz`;
     }
-    else if (num % 5 === 0){.
+    else if (num % 5 === 0){
         li.innerText = `${num}:Buzz`;
     }
     else {
